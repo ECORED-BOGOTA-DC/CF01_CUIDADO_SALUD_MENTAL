4c56bf488429282bcea855134f0034e630de7d3c
Jhon Jairo Urueta Alvarez <jhon.urueta@misena.edu.co>
2023-07-18 09:13:17 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -166,6 +166,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/material.pdf',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
